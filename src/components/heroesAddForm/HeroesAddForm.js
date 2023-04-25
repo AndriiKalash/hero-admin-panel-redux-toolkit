d2e6869bd0c4d66ec53f658c921bdbd9005dfdda
Andrii <andreykalash89@gmail.com>
2023-04-25 17:43:29 -0400

    change for Formik

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,96 +1,123 @@
-import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import store from "../../store";
+import { Formik, Form, Field, ErrorMessage, } from 'formik';
+import * as Yup from 'yup';
 
+import store from "../../store";
 import { selectAll } from "../../components/heroesFilters/filtersSlice";
 import { useCreateHeroMutation } from "../../api/apiSlice";
+import { useSelector } from "react-redux";
+import { filterSelector } from "../../components/heroesFilters/filtersSlice";
+
+const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    description: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    element: Yup.string().required('Chouse the element')
+});
 
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
 // в общее состояние и отображаться в списке + фильтроваться
-// Уникальный идентификатор персонажа можно сгенерировать через uiid
 // Персонаж создается и в файле json при помощи метода POST
 // Дополнительно:
 // Элементы <option></option>  сформировать на базе
 // данных из фильтров
 
+
+
 const HeroesAddForm = () => {
 
     const filters = selectAll(store.getState());
+    const {filtersLoadingStatus} = useSelector(filterSelector);
     const [ addHerro ] = useCreateHeroMutation();
-    const [nameVal, setNameVal]= useState('');
-    const [descriptionVal, setDescriptionVal] = useState('');
-    const [elementVal, setElementVal]= useState('');
+  
    
-
-    const onSubmit = (e) =>{
-         e.preventDefault();
-         if (nameVal, descriptionVal, elementVal ) {
-            const newItem =  {
-                id: uuidv4(),
-                name: nameVal,
-                description: descriptionVal,
-                element: elementVal
-            }
-            addHerro(newItem);   
-         }
-         setNameVal('');
-         setDescriptionVal('');
-         setElementVal(''); 
-    };
-        
     return (
-        <form onSubmit={onSubmit} className="border p-4 shadow-lg rounded">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
-                    onChange={(e)=> setNameVal(e.target.value)}
-                    value={nameVal}
-                    required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
-            </div>
 
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label fs-4">Описание</label>
-                <textarea
-                    onChange={(e)=> setDescriptionVal(e.target.value)}
-                    value={descriptionVal}
-                    required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
-                    placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
-            </div>
-
-            <div className="mb-3">
-                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
-                    onChange={(e)=> setElementVal(e.target.value)}
-                    value={elementVal}
-                    required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
-                    <option >Я владею элементом...</option>
-                    {
-                        filters.filter((elem)=>{
-                            return elem.name !== "all";
-                        }).map((elem)=>{
-                            return (
-                                <option key={elem.id} value={elem.name}>{elem.name}</option>
-                            )
-                        })
-                    }
-                </select>
-            </div>
-
-            <button type="submit" className="btn btn-primary">Создать</button>
-        </form>
+        <Formik 
+        initialValues={{
+            name: '',
+            description: '',
+            element: ''
+            }}
+        validationSchema={SignupSchema}
+        onSubmit={(values, {setSubmitting, resetForm }) => {
+            values.id = uuidv4();
+            addHerro(values);
+            setSubmitting(false);
+            resetForm();
+        }}>
+            {({isSubmitting}) => (
+                <Form className="border p-4 shadow-lg rounded">
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                    <ErrorMessage name="name" component="div" className='error'/>
+                    <Field 
+                        id="name"
+                        name="name"
+                        placeholder="Как меня зовут?"/>
+                </div>
+                <div className="mb-3">
+                    <label 
+                        htmlFor="description"
+                        className="form-label fs-4">Описание
+                    </label>
+                    <ErrorMessage 
+                        name="description"
+                        component="div" 
+                        className='error'/>
+                    <Field 
+                        id="description"
+                        name="description" 
+                        as="textarea"
+                        className="form-control" 
+                        placeholder="Что я умею?"
+                        style={{"height": '130px'}}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                    <ErrorMessage 
+                        name="element"
+                        component="div" 
+                        className='error'/>
+                    <Field 
+                        className="form-select" 
+                        id="element" 
+                        name="element"
+                        as="select">
+                         <option value="" >Я владею элементом...</option>
+                       { 
+                       (filtersLoadingStatus==="loading") ?
+                          ( <div>loading...</div>
+                          ):(
+                            filters.filter((elem)=>{
+                             return elem.name !== "all";
+                            }).map((elem)=>{
+                             return (
+                                 <option 
+                                   key={elem.id} 
+                                   value={elem.name}>{elem.name}</option>
+                                )
+                            })
+                          )
+                         
+                        }  
+                    </Field>
+                </div>
+                <button 
+                   type="submit" 
+                   disabled={isSubmitting} 
+                   className="btn btn-primary">
+                    Создать
+                </button>
+                </Form>
+            )} 
+        </Formik>
     )
 }
 
